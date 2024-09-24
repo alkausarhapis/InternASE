b54c520dcc6288d82d9d09e2ccc4cb36a85f5a23
alkausarhapis <alkausarhapis@gmail.com>
2024-09-24 21:07:51 +0700

    Fix: bootstrap version diff

--- admin/view/footer.php
+++ admin/view/footer.php
@@ -1,8 +1,13 @@
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="assets/js/scripts.js"></script>
-    
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js">
+</script>
+<script src="assets/js/scripts.js"></script>
+
 </body>
 
 </html>
